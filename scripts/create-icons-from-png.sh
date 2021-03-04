bc60619bdb8eeda130317a8d666c3de9a5d4a2be
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-03-04 18:40:17 +1300

    icons and manifest

---
+++ scripts/create-icons-from-png.sh
@@ -0,0 +1,5 @@
+ npx svg-export doc/images/clubs.svg ./public/logo192.png 100% 192:
+ npx svg-export doc/images/clubs.svg ./public/logo192.png 100% 192:
+ npx svg-export doc/images/clubs.svg ./public/favicon.png 100% 16:
+
+ mv ./public/favicon.png ./public/favicon.ico
