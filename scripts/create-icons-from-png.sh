7f7b08f559a033a7a215200ea0b8248fcdfa8a82
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-03-04 19:08:17 +1300

    added eslint. ran eslint with --fix

--- scripts/create-icons-from-png.sh
+++ scripts/create-icons-from-png.sh
@@ -1,5 +1,9 @@
- npx svg-export doc/images/clubs.svg ./public/logo192.png 100% 192:
- npx svg-export doc/images/clubs.svg ./public/logo192.png 100% 192:
- npx svg-export doc/images/clubs.svg ./public/favicon.png 100% 16:
+#!/bin/sh
 
- mv ./public/favicon.png ./public/favicon.ico
+# given an svg file, create relevant icon files
+
+npx svg-export doc/images/clubs.svg ./public/logo512.png 100% 512:
+npx svg-export doc/images/clubs.svg ./public/logo192.png 100% 192:
+npx svg-export doc/images/clubs.svg ./public/favicon.png 100% 16:
+
+mv ./public/favicon.png ./public/favicon.ico
