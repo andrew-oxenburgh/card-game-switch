7585c37ba77a60cbcbd8910f6dc9bf196de12ffb
andrew-oxenburgh <andrew.oxenburgh@gmail.com>
2021-03-04 21:11:08 +1300

    now has PlayingCards

--- src/App.js
+++ src/App.js
@@ -3,13 +3,70 @@
 import './App.css'
 
 import cards from './cards'
+import * as PropTypes from 'prop-types'
+
+const VALUES = {
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    j: 'jack',
+    J: 'jack',
+    q: 'queen',
+    Q: 'queen',
+    k: 'king',
+    K: 'king',
+    a: 'ace',
+    A: 'ace'
+}
+
+PlayingCard.propTypes = {
+    suit: PropTypes.string,
+    value: PropTypes.number | PropTypes.string,
+    back: PropTypes.string
+}
+
+function PlayingCard (props) {
+    const width = 224.22508
+    const height = 312.80777
+    let card
+
+    if (props.back) {
+        if (props.back.toLowerCase().charAt(0) === 'r') {
+            card = 'url("' + cards.redBack + '")'
+        } else {
+            card = 'url("' + cards.blueBack + '")'
+        }
+    } else {
+        let cardValue = props.value
+        const cardSuit = props.suit.charAt(0)
+
+        if (Number.isInteger(cardValue) || cardValue.length === 1) {
+            cardValue = VALUES[cardValue]
+        }
+
+        const cardName = cardValue + cardSuit
+        card = 'url("' + cards[cardName] + '")'
+    }
+    return (
+        <div style={{ width: width, height: height, background: card }}/>
+    )
+}
 
 function App () {
     return (
         <div className="App">
             <h1>Welcome to Switch. The Card Game
-                <div style={{ background: 'url(' + cards.twoC + ')', width: '10em', height: '10em' }}/>
-                <div style={{ background: 'url(' + cards.threeC + ')', width: '10em', height: '10em' }}/>
+                <PlayingCard back="red"/>
+                <PlayingCard back="blue"/>
+                <PlayingCard suit="C" value="ten"/>
+                <PlayingCard suit="D" value={6}/>
             </h1>
         </div>
     )
--- src/cards/index.js
+++ src/cards/index.js
@@ -1,9 +1,114 @@
+import tenC from './10C.svg'
+import tenD from './10D.svg'
+import tenH from './10H.svg'
+import tenS from './10S.svg'
 import twoC from './2C.svg'
+import twoD from './2D.svg'
+import twoH from './2H.svg'
+import twoS from './2S.svg'
 import threeC from './3C.svg'
+import threeD from './3D.svg'
+import threeH from './3H.svg'
+import threeS from './3S.svg'
+import fourC from './4C.svg'
+import fourD from './4D.svg'
+import fourH from './4H.svg'
+import fourS from './4S.svg'
+import fiveC from './5C.svg'
+import fiveD from './5D.svg'
+import fiveH from './5H.svg'
+import fiveS from './5S.svg'
+import sixC from './6C.svg'
+import sixD from './6D.svg'
+import sixH from './6H.svg'
+import sixS from './6S.svg'
+import sevenC from './7C.svg'
+import sevenD from './7D.svg'
+import sevenH from './7H.svg'
+import sevenS from './7S.svg'
+import eightC from './8C.svg'
+import eightD from './8D.svg'
+import eightH from './8H.svg'
+import eightS from './8S.svg'
+import nineC from './9C.svg'
+import nineD from './9D.svg'
+import nineH from './9H.svg'
+import nineS from './9S.svg'
+import aceC from './AC.svg'
+import aceD from './AD.svg'
+import aceH from './AH.svg'
+import aceS from './AS.svg'
+import jackC from './JC.svg'
+import jackD from './JD.svg'
+import jackH from './JH.svg'
+import jackS from './JS.svg'
+import kingC from './KC.svg'
+import kingD from './KD.svg'
+import kingH from './KH.svg'
+import kingS from './KS.svg'
+import queenC from './QC.svg'
+import queenD from './QD.svg'
+import queenH from './QH.svg'
+import queenS from './QS.svg'
+
+import blueBack from './BLUE_BACK.svg'
+import redBack from './RED_BACK.svg'
 
 const cards = {
+    tenC,
+    tenD,
+    tenH,
+    tenS,
     twoC,
-    threeC
+    twoD,
+    twoH,
+    twoS,
+    threeC,
+    threeD,
+    threeH,
+    threeS,
+    fourC,
+    fourD,
+    fourH,
+    fourS,
+    fiveC,
+    fiveD,
+    fiveH,
+    fiveS,
+    sixC,
+    sixD,
+    sixH,
+    sixS,
+    sevenC,
+    sevenD,
+    sevenH,
+    sevenS,
+    eightC,
+    eightD,
+    eightH,
+    eightS,
+    nineC,
+    nineD,
+    nineH,
+    nineS,
+    aceC,
+    aceD,
+    aceH,
+    aceS,
+    jackC,
+    jackD,
+    jackH,
+    jackS,
+    kingC,
+    kingD,
+    kingH,
+    kingS,
+    queenC,
+    queenD,
+    queenH,
+    queenS,
+    blueBack,
+    redBack
 }
 
 export default cards
